a3a98d23441c051657a141412b4c1ab374bd4f81
Zach Pelkey <113135691+zpelkey03@users.noreply.github.com>
2023-02-03 10:57:24 -0700

    Update ex2.4.py

---
+++ ex2/ex2.4.py
@@ -0,0 +1,58 @@
+import sys, json, time, matplotlib.pyplot as plt, pandas as pd, timeit, numpy as np
+sys.setrecursionlimit(20000)
+
+def func1(arr, low, high):
+    if low < high:
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
+    else:
+        insertion_sort(arr, low, high)
+
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+
+def insertion_sort(arr, low, high):
+    for i in range(low, high + 1):
+        current = arr[i]
+        j = i - 1
+        while j >= low and arr[j] > current:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = current
+
+with open('ex2.json', 'r') as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data)
+
+timings = {}
+
+    
+for key in df.columns:
+    if np.isnan(df[key]).any():
+         continue
+    array = df[key].to_list()
+    time = timeit.timeit(lambda: func1(array, 0, len(array) - 1), number=1)
+    timings[key] = time
+
+x = list(timings.keys())
+y = list(timings.values())
+plt.plot(x, y)
+plt.xlabel('Array')
+plt.ylabel('Time (s)')
+plt.title('Quicksort Timing Results')
+plt.show()
